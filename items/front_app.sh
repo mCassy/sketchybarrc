eedcaa24463387a4a70ac58d26fb012a58d14a94
Michael <misha.krasilynets@gmail.com>
2024-01-15 18:25:50 +0200

    restore

---
+++ items/front_app.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+sketchybar \
+	--add item front_app left \
+	--set front_app script="$PLUGIN_DIR/front_app.sh" \
+	icon.drawing=off \
+	icon.font="$FONT:Bold:15.0" \
+	background.padding_left=0 \
+	background.padding_right=10 \
+	label.color=$PINE \
+	label.font="$FONT:Bold:15.0" \
+	associated_display=active \
+	--subscribe front_app front_app_switched
